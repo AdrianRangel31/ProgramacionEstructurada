cb18caffc23594f841a4e5c4ed667a020f4c9dcc
AdrianRangel31 <luis_3141240117@utd.edu.mx>
2025-07-01 09:03:15 -0600

    Delete Parcial1 directory

--- Parcial1/2_modulos_paquetes/utilizar_modulos.py
+++
@@ -1,7 +0,0 @@
-import modulos
-modulos.borrarPantalla()
-print(modulos.saludos("Adrián"))
-
-from modulos import saludos, borrarPantalla
-borrarPantalla()
-saludos("Adrian")
--- Parcial1/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,6 +0,0 @@
-from paquete1 import modulos
-print(modulos.saludos("Adrian"))
-
-modulos.borrarPantalla()
-string = modulos.solicitar()
-print(string)
